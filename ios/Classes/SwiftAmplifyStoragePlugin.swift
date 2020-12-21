00007d0c43adc848764edc362e02641fa122dc77
Daniel Quick <danielquick14@gmail.com>
2020-12-21 16:49:29 -0600

    ios updates

--- ios/Classes/SwiftAmplifyStoragePlugin.swift
+++ ios/Classes/SwiftAmplifyStoragePlugin.swift
@@ -1,14 +1,72 @@
 import Flutter
 import UIKit
+import Amplify
+import AmplifyPlugins
 
 public class SwiftAmplifyStoragePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amplify_storage", binaryMessenger: registrar.messenger())
-    let instance = SwiftAmplifyStoragePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "amplify_storage", binaryMessenger: registrar.messenger())
+        let instance = SwiftAmplifyStoragePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result(FlutterMethodNotImplemented)
-  }
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+            case "initialize":
+                initialize(result: result)
+            case "uploadFile":
+                uploadFile(call: call, result: result)
+            default:
+                result(FlutterMethodNotImplemented);
+        }
+    }
+    
+    func initialize(result: @escaping FlutterResult) {
+        do {
+            try Amplify.add(plugin: AWSS3StoragePlugin())
+            result(true)
+        } catch {
+            result(FlutterError(code: "", message: error.localizedDescription, details: nil))
+        }
+    }
+    
+    func uploadFile(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if let arguments = call.arguments as? NSDictionary {
+        
+            var path = ""
+            var key = ""
+            var data: Data!
+
+            if let arg = arguments["path"], let val = arg as? String {
+                path = val
+                let url = URL(fileURLWithPath: path)
+                do {
+                    data = try Data(contentsOf: url)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            if let arg = arguments["key"], let val = arg as? String {
+                key = val
+            }
+            
+            Amplify.Storage.uploadData(key: key, data: data,
+                                       progressListener: {progress in },
+                                       resultListener: { (event) in
+                    switch event {
+                        case .success(let data):
+                            print("success")
+                            DispatchQueue.main.async {
+                                result(true)
+                            }
+                        case .failure(let storageError):
+                            print("failure")
+                        DispatchQueue.main.async {
+                            result(FlutterError(code: "", message: storageError.localizedDescription, details: nil))
+                        }
+                    }
+            })
+        }
+    }
 }
